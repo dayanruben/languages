480d9c5587d3587f0fec409d26d0e89ef5ac5701
Dan Fingal-Surma <dfingal@gmail.com>
2025-08-16 01:29:43 -0700

    Adds Kotlin benchmarking support
    Adds a run.kt file for each benchmark and support in languages.sh for compiling and running this file.
    
    This re-uses the Benchmark library in lib/java/languages.

---
+++ loops/kotlin/run.kt
@@ -0,0 +1,23 @@
+import languages.Benchmark
+import kotlin.random.Random
+
+fun loops(u: Int): Int{
+    val r: Int = Random.nextInt(10_000)
+    val a = IntArray(10_000)
+    for (i in 0..<10_000) {
+        for (j in 0..<10_000) {
+            a[i] = a[i] + j % u
+        }
+        a[i] = a[i] + r
+    }
+    return a[r]
+}
+
+fun main(args: Array<String>) {
+    val runMs = args[0].toLong()
+    val warmupMS = args[1].toLong()
+    val n = args[2].toInt()
+    Benchmark.run( { loops(n) }, warmupMS);
+    val results = Benchmark.run( { loops(n) }, runMs);
+    println(Benchmark.formatResults(results));
+}
